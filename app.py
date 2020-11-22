7890d216d19cd2322e55dd809a0815724df1ae72
Phillip A Pearson <phillip.pearson@live.co.uk>
2020-11-22 00:11:31 +0000

    contiued to add documentation to register, login, and app.py files

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+# This sets up our Environment variables in order to run our web app
+# and access the database at MongoDB Atlas.
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -22,27 +24,34 @@ if __name__ == "__main__":
             port=int(os.environ.get("PORT")),
             debug=True)
 
+# This sets up our basic routing through our apps folder structure.
 @app.route("/")
+
+# Routes to our Login template
 @app.route("/login")
+
+# Allows the user to login with their pre-existing username and password.
 def login():
     return render_template("login.html")
 
 
+# This allows the user to no longer be the active-user, and removes session cookies.
 @app.route("/logout")
 def logout():
     return redirect(render_template("login.html"))
 
-
+# Routes to our Register template, and allows the user to create a new username
+# and password.
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-
+# Routes to the create a character profile template.
 @app.route("/create")
 def create():
     return render_template("create.html")
 
-
+# Displays a list of created character profiles from the database.
 @app.route("/characters")
 def characters():
     return render_template("characters.html")
