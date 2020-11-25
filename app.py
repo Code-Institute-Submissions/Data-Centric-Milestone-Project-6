e3ff739e7f810d0d6bfe2643934bb4ac2ba09c72
Phillip A Pearson <phillip.pearson@live.co.uk>
2020-11-25 23:11:28 +0000

    completed login, logout, and register functions in app.py, altered from tags to included 'GET' and 'POST' Methods, in register.html and login.html templates. added a little bit of UX to included a 'Logged in as' message in the top right corner of the sites pages, when a session user is active.

--- app.py
+++ app.py
@@ -28,24 +28,72 @@ if __name__ == "__main__":
 # This sets up our basic routing through our apps folder structure.
 @app.route("/")
 
-# Routes to our Login template
-@app.route("/login")
+# Routes to our Register template, and allows the user to create a new username
+# and password.
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if user exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("characters", username=session["user"]))
+
+    return render_template("register.html")
+
+
+# Routes to our Login template.
 # Allows the user to login with their pre-existing username and password.
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # Checks if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Checks if the hashed password (in the Monogo Database),
+            # matches user input.
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}!".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "characters", username=session["user"]))
+            else:
+                # In event of an incorrect password input.
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # If the Username input by user doesn't exist, flash message and redirect.
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
 # This allows the user to no longer be the active-user, and removes session cookies.
 @app.route("/logout")
 def logout():
-    return redirect(render_template("login.html"))
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
-# Routes to our Register template, and allows the user to create a new username
-# and password.
-@app.route("/register")
-def register():
-    return render_template("register.html")
 
 # Routes to the create a character profile template.
 @app.route("/create")
