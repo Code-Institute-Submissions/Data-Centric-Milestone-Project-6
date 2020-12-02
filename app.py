4a9d435229391b0d09f1ae8823d584d6f30680e5
Phillip A Pearson <59122457+phillpearsondev@users.noreply.github.com>
2020-12-02 23:36:09 +0000

    updated app.py in attempt to fix 404 error via heroku

--- app.py
+++ app.py
@@ -31,10 +31,9 @@ if __name__ == "__main__":
             debug=True)
 
 # This sets up our basic routing through our apps folder structure.
-@app.route("/")
-
 # Routes to our Register template, and allows the user to create a new username
 # and password.
+@app.route("/")
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
