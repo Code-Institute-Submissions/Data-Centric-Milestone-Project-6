10eb8c5a210d5bc298137ebbf9eb627c84d563c2
Phillip A Pearson <59122457+phillpearsondev@users.noreply.github.com>
2020-12-09 17:10:13 +0000

    deleted env.py from github
    in order to keep secret keys secret

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "mysecretstring1234")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://phillpearsondev:el2vhwVW4j0ORGdd@cluster0.h5e9p.mongodb.net/character_profiler?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "character_profiler")
